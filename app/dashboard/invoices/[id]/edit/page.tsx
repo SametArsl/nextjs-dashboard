6a8efc2c566c7de398dc72accdb6a8460e9623a1
sametarsl <sametarsl@gmail.com>
2023-12-14 17:17:23 +0300

    completed next.js learn

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,24 +1,29 @@
-import Form from "@/app/ui/invoices/edit-form";
-import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
-import { fetchInvoiceById, fetchCustomers } from "@/app/lib/data";
-import { notFound } from "next/navigation";
+import Form from '@/app/ui/invoices/edit-form';
+import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
+import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
+import { notFound } from 'next/navigation';
+import { Metadata } from 'next';
 
-export default async function Page({ params }: { params: {id: string } }){
+export const metadata: Metadata = {
+    title: 'Edit Invoice',
+};
+
+export default async function Page({ params }: { params: { id: string } }) {
     const id = params.id;
     const [invoice, customers] = await Promise.all([
         fetchInvoiceById(id),
         fetchCustomers(),
     ]);
 
-    if(!invoice){
+    if (!invoice) {
         notFound();
     }
 
     return (
         <main>
-            <Breadcrumbs 
+            <Breadcrumbs
                 breadcrumbs={[
-                    {label: 'Invoices', href: '/dashboard/invoices'},
+                    { label: 'Invoices', href: '/dashboard/invoices' },
                     {
                         label: 'Edit Invoice',
                         href: `/dashboard/invoices/${id}/edit`,
@@ -26,7 +31,7 @@ export default async function Page({ params }: { params: {id: string } }){
                     },
                 ]}
             />
-            <Form invoice={invoice} customers={customers}/>
+            <Form invoice={invoice} customers={customers} />
         </main>
-    )
+    );
 }
